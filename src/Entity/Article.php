3e7a5f12eb1b8cfeac958333cecfddeccbd824f4
Dhana <dhanasekar@goldenscent.com>
2024-04-25 15:53:58 +0400

    Initial commit: Adding all project files

---
+++ src/Entity/Article.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $no_of_licence;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNoOfLicence(): ?int
+    {
+        return $this->no_of_licence;
+    }
+
+    public function setNoOfLicence(int $no_of_licence): self
+    {
+        $this->no_of_licence = $no_of_licence;
+
+        return $this;
+    }
+}
